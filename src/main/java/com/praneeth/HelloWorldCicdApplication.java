a77ba214ffca210a5af7d148c3102f75bf46fa17
praneeth <praneethreddy8498995302@gmail.com>
2021-11-06 19:57:40 +0530

    intial commits

---
+++ src/main/java/com/praneeth/HelloWorldCicdApplication.java
@@ -0,0 +1,13 @@
+package com.praneeth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldCicdApplication.class, args);
+	}
+
+}
