90b71794b48b0812d6a724fd338f3e7fe2d02716
praneeth <praneethreddy8498995302@gmail.com>
2021-11-07 23:53:20 +0530

    added log

--- src/main/java/com/praneeth/controller/HelloWorldController.java
+++ src/main/java/com/praneeth/controller/HelloWorldController.java
@@ -1,16 +1,19 @@
 package com.praneeth.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class HelloWorldController {
 
     @GetMapping
 public String helloWorld()
 {
+    log.info("hello world method started");
     return "hello world";
 }
 }
